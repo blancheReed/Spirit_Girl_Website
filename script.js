e370c0b808ca9c399dd00a58ec6d4da0a03ba1d1
Blanche Reed <iworldc.ff@gmail.com>
2023-09-10 19:07:48 +1000

    Updating work done on 10 Sept in script.js file

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const mobileNav = document.querySelector(".mobile-nav");
 hamburgerButton.addEventListener("click", () => {
   mobileNav.classList.toggle("display-none");
 });
+
+// const charactersButton = document.querySelector(".characters-button");
+// const mobileNav2 = document.querySelector(".mobile-nav2");
+
+// charactersButton.addEventListener("click", () => {
+//   mobileNav2.classList.toggle("display-none");
+// });
